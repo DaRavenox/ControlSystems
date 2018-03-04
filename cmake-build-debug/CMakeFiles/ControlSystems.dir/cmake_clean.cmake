af05bddfc438fb49a5125250c2365b44a879eddb
DaRavenox <hongisto@kth.se>
2018-03-04 14:25:25 +0100

    new try

---
+++ cmake-build-debug/CMakeFiles/ControlSystems.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ControlSystems.dir/main.c.o"
+  "ControlSystems.pdb"
+  "ControlSystems"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ControlSystems.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ControlSystems.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ControlSystems.
+# This may be replaced when dependencies are built.
